c69737cc113daed575613ec248d79faf61515915
peter <pedro.bustamante1832@gmail.com>
2023-04-26 17:37:18 -0500

    trabajo para hacer +2

--- intro/comentar_trabajo_profe/whil3.py
+++ intro/comentar_trabajo_profe/whil3.py
@@ -2,19 +2,21 @@
 # print(type(num))
 # num="sena"
 # print(type(num))
-num=1
-cont=0
-sum=0
-menor=1000000
-mayor=0
-while num!=0:
-    num=int(input('ingrese numero'))
-    cont=cont+1
-    sum=sum+num
-    if num>mayor:
-        mayor=num
-    if num<menor and num!=0:
-        menor=num
+num = 1  # se crea variable num que sera igual a 1, para almacenar numero.
+
+# se crea variable cont que sera igual a 0, para almacenar un contador .
+cont = 0
+sum = 0  # se crea variable sum que sera igual a 0, para almacenar una suma .
+menor = 1000000  # se crea variable menor para almacenar el numero menor.
+mayor = 0  # crea variable mayor, para almacenar el numero mayor .
+while num != 0:
+    num = int(input('ingrese numero'))
+     cont = cont+1
+      sum = sum+num
+       if num > mayor:
+            mayor = num
+        if num < menor and num != 0:
+            menor = num
 
 print(f'fueron ingresados {cont-1} numeros')
 print(f'La suma es {sum}')
