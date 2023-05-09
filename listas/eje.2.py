850bbd73dd2c62c9af9f14a48ac36e5cb04056c6
peter <pedro.bustamante1832@gmail.com>
2023-05-09 15:22:27 -0500

    trabajo de listas48

--- listas/eje.2.py
+++ listas/eje.2.py
@@ -4,12 +4,27 @@
 #el curso puede tener entre 20 y 30 aprendise.
 
 import random
-
-calificaciones=[round(random.random()*5,2) for x in range(random.randint(20,31))]
+tam=(random.randint(20,31))
+calificaciones=[round(random.random()*5,2) for x in range(tam)]
 
 print(calificaciones)
 #hallar
 #genere lista nuevas para los aprobados y reprobados (se aprueba con 3)
-resultado=["aprobado" if x>3 else 0 for x in calificaciones]
+resultado=[(x,"aprobado") for x in calificaciones if x>3 ]
 print(resultado)
+resultado2=[(x,"reprobado") for x in calificaciones if x<3 ]
+print(resultado2)
+# #generar lista nueva de
+
+
+var=["reprobado" if x < 3 else "aprovado" for x in calificaciones]
+print(var)
+for q in range(tam):
+    for i in range(q+1,tam):
+        if calificaciones[q] > calificaciones[i]:
+            calificaciones[q], calificaciones[i] = calificaciones[i], calificaciones[q]
+print(calificaciones)
+print(resultado)
+resultado2=[(x,"reprobado") for x in calificaciones if x<3 ]
+print(resultado2)
 
--- listas/ejercio_clase.py
+++ listas/ejercio_clase.py
@@ -3,6 +3,7 @@
 import random
 lista=[random.randrange(100)for i in range(random.randint(5,10))]
 print(lista)
+
 par=[x**2 if x%2==0 else x for x in lista]
 
 print(par)
