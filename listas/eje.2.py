dd3e0e1d9f18a6d54a90426ae53c8bf29f055c57
peterbustamante <106874805+peterbustamante@users.noreply.github.com>
2023-05-10 09:50:41 -0500

    realizacion de eje 2

--- listas/eje.2.py
+++ listas/eje.2.py
@@ -10,21 +10,39 @@ calificaciones=[round(random.random()*5,2) for x in range(tam)]
 print(calificaciones)
 #hallar
 #genere lista nuevas para los aprobados y reprobados (se aprueba con 3)
-resultado=[(x,"aprobado") for x in calificaciones if x>3 ]
-print(resultado)
-resultado2=[(x,"reprobado") for x in calificaciones if x<3 ]
-print(resultado2)
+# resultado=[(x,"aprobado") for x in calificaciones if x>3 ]
+# print(resultado)
+# resultado2=[(x,"reprobado") for x in calificaciones if x<3 ]
+# print(resultado2)
 # #generar lista nueva de
 
+prom=0
+prom2=0
 
-var=["reprobado" if x < 3 else "aprovado" for x in calificaciones]
-print(var)
 for q in range(tam):
     for i in range(q+1,tam):
         if calificaciones[q] > calificaciones[i]:
             calificaciones[q], calificaciones[i] = calificaciones[i], calificaciones[q]
+            if calificaciones[i] <=3:
+                prom+=1
+            if calificaciones[i] >=3:
+                prom2+=1
+                
+            
 print(calificaciones)
-print(resultado)
-resultado2=[(x,"reprobado") for x in calificaciones if x<3 ]
-print(resultado2)
+
+var=["reprobado" if x < 3 else "aprovado" for x in calificaciones]
+l=4
+print(var)
+grupo1 =[x for x in calificaciones if 1>=x  ]
+print(grupo1)
+grupo2 =[x for x in calificaciones if 2>=x and x>=1 ]
+print(grupo2)
+grupo3 =[x for x in calificaciones if 3>=x and x>=2 ]
+print(grupo3)
+grupo4 =[x for x in calificaciones if 4>=x and x>=3 ]
+print(grupo4)
+grupo5 =[x for x in calificaciones if 5>=x and x>=4 ]
+print(grupo5)
+print(prom)
 
