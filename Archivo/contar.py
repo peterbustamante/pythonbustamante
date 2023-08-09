f14d51c72a87e533f62a47dc0d6b14ce8c69c2b0
peterbustamante <106874805+peterbustamante@users.noreply.github.com>
2023-08-09 10:36:13 -0500

    contar

--- Archivo/contar.py
+++ Archivo/contar.py
@@ -1,15 +1,18 @@
 #cuantas letras tiene cada linea del hino de soacha escribir la respuesta en otro archvo
 try:
-    arch=open('C:\\pythonbustamante\\Archivo\\himno.txt', 'r',encoding='utf-8')
+    arch=open('C:\\carpeta clon\\pythonbustamante\\Archivo\\himno.txt', 'r',encoding='utf-8')
     cont=1
     linea=arch.readline()
     caracter=0
 
     while linea !='':
-        print(f'{linea}{caracter}')
-        linea=arch.readline()
         for i in linea:
             caracter+=1
+        print(f'{linea} ={caracter}')
+        
+        linea=arch.readline()
+        caracter = 0
+
 
 
 
